c715106ea1a8d03173d008052d4ce8e938c30de4
Vinicius-Justino <justinovinicius2006@gmail.com>
2024-04-11 16:54:20 -0300

    todos os botões funcionam e menu de opções animado

--- js/hud.js
+++ js/hud.js
@@ -2,5 +2,16 @@ function mostra_opcoes() {
     let menu_opcoes = document.getElementById("menu-opcoes");
 
     menu_opcoes.style.left = (menu_opcoes.style.left == "") ? "0px" : "";
-    console.log("foi");
+}
+
+function troca_ferramenta(ferramenta) {
+    document.getElementById("ferramenta-atual").value = ferramenta;
+}
+
+function troca_cor(cor) {
+    document.getElementById("cor-atual").value = cor;
+}
+
+function troca_status(status) {
+    document.getElementById("status-atual").value = status;
 }
--- js/jogo.js
+++ js/jogo.js
@@ -1,7 +1,23 @@
 let canvasJogo;
-
 function inicializa_canvas() {
     canvasJogo = document.getElementById("canvas-jogo");
     canvasJogo.height = window.innerHeight;
     canvasJogo.width = window.innerWidth;
+}
+
+let ferramentaAtual = "lapis";
+function atualiza_ferramenta() {
+    ferramentaAtual = document.getElementById("ferramenta-atual").value;
+    console.log(ferramentaAtual);
+}
+
+let corAtual = "azul";
+function atualiza_cor() {
+    corAtual = document.getElementById("cor-atual").value;
+    console.log(corAtual);
+}
+
+let statusTempo = "stop";
+function atualiza_status() {
+    statusTempo = document.getElementById("status-atual").value;
 }
